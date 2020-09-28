2f653e6109c1b143b0176e388d84b86967b8ed1c
naipaka <naipakapaka@gmail.com>
2020-09-28 10:39:03 +0900

    Initial Commit

---
+++ NextSunnyDay/NextSunnyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextSunnyDayApp.swift
+//  NextSunnyDay
+//
+//  Created by rMac on 2020/09/28.
+//
+
+import SwiftUI
+
+@main
+struct NextSunnyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
