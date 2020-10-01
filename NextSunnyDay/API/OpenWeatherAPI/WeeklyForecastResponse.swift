dba05cf6f3e1c92cd0720c80fcec4bee10ba70d7
naipaka <naipakapaka@gmail.com>
2020-10-01 17:17:46 +0900

    Define weekly forecast response for WeatherOpenApi

---
+++ NextSunnyDay/API/OpenWeatherAPI/WeeklyForecastResponse.swift
@@ -0,0 +1,93 @@
+//
+//  Responses.swift
+//  NextSunnyDay
+//
+//  Created by rMac on 2020/10/01.
+//
+
+import Foundation
+
+// MARK: - WeeklyForecastResponse
+struct WeeklyForecastResponse: Codable {
+    let lat: Double
+    let lon: Double
+    let timezone: String
+    let timezoneOffset: Int
+    let daily: [Daily]
+
+    enum CodingKeys: String, CodingKey {
+        case lat
+        case lon
+        case timezone
+        case timezoneOffset = "timezone_offset"
+        case daily
+    }
+
+    // MARK: - Daily
+    struct Daily: Codable {
+        let date: Int
+        let sunrise: Int
+        let sunset: Int
+        let temp: Temp
+        let feelsLike: FeelsLike
+        let pressure, humidity: Int
+        let dewPoint, windSpeed: Double
+        let windDeg: Int
+        let weather: [Weather]
+        let clouds: Int
+        let pop: Double
+        let rain: Double?
+        let uvi: Double
+
+        enum CodingKeys: String, CodingKey {
+            case date = "dt"
+            case sunrise
+            case sunset
+            case temp
+            case feelsLike = "feels_like"
+            case pressure
+            case humidity
+            case dewPoint = "dew_point"
+            case windSpeed = "wind_speed"
+            case windDeg = "wind_deg"
+            case weather
+            case clouds
+            case pop
+            case rain
+            case uvi
+        }
+
+        // MARK: - Temp
+        struct Temp: Codable {
+            let day: Double
+            let min: Double
+            let max: Double
+            let  night: Double
+            let eve: Double
+            let  morn: Double
+        }
+
+        // MARK: - FeelsLike
+        struct FeelsLike: Codable {
+            let day: Double
+            let night: Double
+            let eve: Double
+            let morn: Double
+        }
+
+        // MARK: - Weather
+        struct Weather: Codable {
+            let id: Int
+            let main: String
+            let weatherDescription: String
+            let icon: String
+
+            enum CodingKeys: String, CodingKey {
+                case id
+                case main
+                case weatherDescription = "description"
+                case icon
+            }
+        }
+    }
+}
