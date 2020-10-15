2cf1561786c9f286dd44cf1b12e85f77c0b8b5f1
naipaka <naipakapaka@gmail.com>
2020-10-15 17:48:28 +0900

    Fix MKLocalSearchCompletion to be compliant with Identifiable

--- NextSunnyDay/API/LocalSearch/LocalSearchService.swift
+++ NextSunnyDay/API/LocalSearch/LocalSearchService.swift
@@ -28,3 +28,5 @@ class LocalSearchService: NSObject, ObservableObject, MKLocalSearchCompleterDele
         completions = completer.results
     }
 }
+
+extension MKLocalSearchCompletion: Identifiable {}
