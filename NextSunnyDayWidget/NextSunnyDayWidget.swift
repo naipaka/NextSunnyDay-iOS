f41e0716b12ef48551aea8925e0a531f6af35f0a
naipaka <naipakapaka@gmail.com>
2020-10-21 20:06:52 +0900

    Implemented automatic reloading of the Widget

--- NextSunnyDayWidget/NextSunnyDayWidget.swift
+++ NextSunnyDayWidget/NextSunnyDayWidget.swift
@@ -7,8 +7,13 @@
 
 import WidgetKit
 import SwiftUI
+import Combine
+
+private var cancellables = Set<AnyCancellable>()
 
 struct Provider: TimelineProvider {
+    private let weatherFetcher = WeatherFetcher()
+
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), entity: DailyWeatherForecastEntity.defaultEntity())
     }
@@ -21,14 +26,35 @@ struct Provider: TimelineProvider {
     }
 
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        let currentDate = Date()
         let results = DailyWeatherForecastEntity.all()
         let entity = results.first ?? DailyWeatherForecastEntity()
-        let entry = SimpleEntry(date: Date(), entity: entity)
-        entries.append(entry)
 
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
+        guard let entryDate = Calendar.current.date(byAdding: .hour, value: 5, to: currentDate) else { return }
+        let timeline = Timeline(entries: [SimpleEntry(date: currentDate, entity: entity)], policy: .after(entryDate))
+
+        let latestDate = entity.daily.min(by: { $0.date < $1.date })?.date ?? 0
+        if !entity.cityName.isEmpty && latestDate + 60 * 60 * 24 < Int(currentDate.timeIntervalSince1970) {
+            weatherFetcher.weeklyWeatherForecast(forLat: entity.lat, forLon: entity.lon)
+                .receive(on: RunLoop.main)
+                .sink(
+                    receiveCompletion:  { value in
+                        switch value {
+                        case .failure:
+                            completion(timeline)
+                        case .finished:
+                            break
+                        }
+                    },
+                    receiveValue: { forecast in
+                        DailyWeatherForecastEntity.update(with: DailyWeatherForecastEntity.generateEntity(cityName: entity.cityName, forecast: forecast))
+                        completion(timeline)
+                    }
+                )
+                .store(in: &cancellables)
+        } else {
+            completion(timeline)
+        }
     }
 }
 
@@ -39,7 +65,7 @@ struct SimpleEntry: TimelineEntry {
 
 struct NextSunnyDayWidgetEntryView : View {
     var entry: Provider.Entry
-    
+
     @Environment(\.widgetFamily) var family
 
     var body: some View {
